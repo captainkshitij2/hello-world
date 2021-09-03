23f6695d7d59b5be450f98258e4cf187274e7aa1
kshitij <kshitij4242@gmail.com>
2021-09-04 00:06:33 +0530

    added

--- recursion/q1.js
+++ recursion/q1.js
@@ -41,4 +41,39 @@ else
 	console.log("False ");
 /////////////
 
+//2nd
 
+function convertToBinary(x) {
+    let bin = 0;
+    let rem, i = 1, step = 1;
+    while (x != 0) {
+        rem = x % 2;
+        console.log(
+            `Step ${step++}: ${x}/2, Remainder = ${rem}, Quotient = ${parseInt(x/2)}`
+        );
+        x = parseInt(x / 2);
+        bin = bin + rem * i;
+        i = i * 10;
+    }
+    console.log(`Binary: ${bin}`);
+}
+let number = 7
+convertToBinary(number);
+
+///3rd
+
+
+function gcd_two_numbers(x, y) {
+    if ((typeof x !== 'number') || (typeof y !== 'number')) 
+      return false;
+    x = Math.abs(x);
+    y = Math.abs(y);
+    while(y) {
+      var t = y;
+      y = x % y;
+      x = t;
+    }
+    return x;
+  }
+  
+  console.log(gcd_two_numbers(6, 9));
